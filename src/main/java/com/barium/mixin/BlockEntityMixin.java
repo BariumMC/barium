c4affa918d5aa1e7eb38c6fbb6cfe21e41ce9755
PedrixzZDev <pedrixzz6@gmail.com>
2025-05-20 16:00:05 -0300

    Update BlockEntityMixin.java

--- src/main/java/com/barium/mixin/BlockEntityMixin.java
+++ src/main/java/com/barium/mixin/BlockEntityMixin.java
@@ -3,7 +3,7 @@ package com.barium.mixin;
 import com.barium.optimization.BlockTickOptimizer;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.HopperBlockEntity;
-import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.block.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
