d82446104a682073c6573cf3c7a0b2d538062468
PedrixzZDev <pedrixzz6@gmail.com>
2025-05-21 19:42:33 +0000

    fix

--- src/client/java/com/barium/client/mixin/InGameHudMixin.java
+++ src/client/java/com/barium/client/mixin/InGameHudMixin.java
@@ -3,9 +3,12 @@ package com.barium.client.mixin;
 import com.barium.client.optimization.HudOptimizer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
