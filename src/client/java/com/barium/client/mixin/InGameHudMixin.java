0b1b2ad7bb5bbf22bd16c8cc28ded6b5c31cbda0
PedrixzZDev <pedrixzz6@gmail.com>
2025-05-21 16:15:06 -0300

    Update InGameHudMixin.java

--- src/client/java/com/barium/client/mixin/InGameHudMixin.java
+++ src/client/java/com/barium/client/mixin/InGameHudMixin.java
@@ -1,6 +1,9 @@
 package com.barium.client.mixin;
 
 import com.barium.client.optimization.HudOptimizer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.util.RenderTickCounter;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
