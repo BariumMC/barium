15ed57e0987b1c05b5bef89524c753f8a1055411
PedrixzZDev <pedrixzz6@gmail.com>
2025-05-21 19:37:59 +0000

    fix

--- src/client/java/com/barium/client/mixin/InGameHudMixin.java
+++ src/client/java/com/barium/client/mixin/InGameHudMixin.java
@@ -2,30 +2,26 @@ package com.barium.client.mixin;
 
 import com.barium.client.optimization.HudOptimizer;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.RenderTickCounter;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
 
-     static {
-        System.out.println("MixinInGameHud aplicado com sucesso!");
+    @Redirect(
+        method = "render",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/hud/InGameHud;renderStatusEffectOverlay(Lnet/minecraft/client/gui/DrawContext;)V"
+        )
+    )
+    private void redirectRenderStatusEffectOverlay(InGameHud instance, DrawContext context) {
+        if (HudOptimizer.shouldUpdateHudElement("status_effects", () -> "effects")) {
+            ((InGameHudAccessor) instance).barium$invokeRenderStatusEffectOverlay(context);
+        }
     }
-    
-@Redirect(method = "render", 
-    at = @At(value = "INVOKE", 
-             target = "Lnet/minecraft/client/gui/hud/InGameHud;renderStatusEffectOverlay(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/util/RenderTickCounter;)V"))
-private void redirectRenderStatusEffectOverlay(InGameHud instance, DrawContext context, RenderTickCounter tickCounter) {
-    if (HudOptimizer.shouldUpdateHudElement("status_effects", () -> "effects")) {
-        instance.renderStatusEffectOverlay(context, tickCounter);
-    }
-}
     
     @Inject(method = "renderStatusBars", at = @At("HEAD"), cancellable = true)
     private void onRenderStatusBars(MatrixStack matrices, CallbackInfo ci) {
