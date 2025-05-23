d0008a68823a6d3ede06ee1ca411acde963a0e5f
PedrixzZDev <pedrixzz6@gmail.com>
2025-05-23 15:47:49 +0000

    fix

--- src/client/java/com/barium/client/mixin/BuiltChunkMixin.java
+++ src/client/java/com/barium/client/mixin/BuiltChunkMixin.java
@@ -6,19 +6,16 @@ import net.minecraft.client.render.chunk.ChunkBuilder.BuiltChunk;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.chunk.WorldChunk;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import com.barium.client.mixin.accessor.BuiltChunkAccessor; // Import the accessor
 
 @Mixin(BuiltChunk.class)
 public abstract class BuiltChunkMixin {
 
-    // Removed the @Shadow field since we're using an Accessor now
-    // @Shadow @Final private BlockPos origin;
+    // No need for @Shadow or @Accessor here, as we'll call a public method.
+    // The BuiltChunk class (which this mixin applies to) already has a public `getOrigin()` method.
 
     @Inject(method = "rebuild", at = @At("HEAD"), cancellable = true)
     private void barium$onRebuild(CallbackInfoReturnable<Runnable> cir) {
@@ -27,9 +24,9 @@ public abstract class BuiltChunkMixin {
             return;
         }
 
-        // Use the accessor to get the origin BlockPos
-        BuiltChunkAccessor accessor = (BuiltChunkAccessor) this;
-        BlockPos originPos = accessor.getOriginPos();
+        // Get the BuiltChunk instance (this) and call its public getOrigin() method.
+        // The cast to BuiltChunk is often needed for the compiler when dealing with Mixin's `this`.
+        BlockPos originPos = ((BuiltChunk)(Object)this).getOrigin();
 
         ChunkPos chunkPos = new ChunkPos(originPos.getX() >> 4, originPos.getZ() >> 4);
 
