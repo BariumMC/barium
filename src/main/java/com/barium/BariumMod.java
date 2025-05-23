9d0bdf6a197726a24ee1a098a3762129b3b86187
PedrixzZDev <pedrixzz6@gmail.com>
2025-05-23 23:10:28 +0000

    fix

--- src/main/java/com/barium/BariumMod.java
+++ src/main/java/com/barium/BariumMod.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.barium.config.BariumConfig;
-import com.barium.optimization.*;
 import me.shedaniel.autoconfig.AutoConfig; // Importe para AutoConfig
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer; // Importe para o serializador
 
@@ -19,15 +18,6 @@ public class BariumMod implements ModInitializer {
         
         // Registrar as configurações do Cloth Config
         BariumConfig.registerConfigs();
-        
-        // Inicializar sistemas de otimização server-side
-        PathfindingOptimizer.init();
-        BlockTickOptimizer.init();
-        RedstoneOptimizer.init();
-        EntityTickOptimizer.init();
-        InventoryOptimizer.init();
-        ChunkSavingOptimizer.init();
-        ServerTerrainOptimizer.init(); 
         
         LOGGER.info("Barium inicializado com sucesso!");
     }
