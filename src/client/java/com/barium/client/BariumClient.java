bf442ccf932ffd19cec89bcc55dd8c0029b990e2
PedrixzZDev <pedrixzz6@gmail.com>
2025-05-24 20:01:44 +0000

    test

--- src/client/java/com/barium/client/BariumClient.java
+++ src/client/java/com/barium/client/BariumClient.java
@@ -4,7 +4,6 @@ import com.barium.BariumMod;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import com.barium.client.optimization.HudOptimizer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents; // Import para eventos de desconexão
 
 @Environment(EnvType.CLIENT)
@@ -12,13 +11,9 @@ public class BariumClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         BariumMod.LOGGER.info("Inicializando cliente Barium");
-        
-        // Inicialização dos otimizadores client-side
-        HudOptimizer.init(); // Otimizador de HUD e Texto
 
         // Registrar eventos para limpar caches em situações apropriadas
         ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
-            HudOptimizer.clearHudCache(); // Limpa o cache do HUD (incluindo Debug HUD)
             BariumMod.LOGGER.debug("Caches de cliente limpos devido a desconexão.");
         });
 
