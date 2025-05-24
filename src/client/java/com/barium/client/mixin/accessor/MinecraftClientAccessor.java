db4e73cd22852bf45bdf1b5d4470f46c3fc4edc0
PedrixzZDev <pedrixzz6@gmail.com>
2025-05-24 01:02:13 +0000

    fix

--- src/client/java/com/barium/client/mixin/accessor/MinecraftClientAccessor.java
+++ src/client/java/com/barium/client/mixin/accessor/MinecraftClientAccessor.java
@@ -1,12 +1,12 @@
 package com.barium.client.mixin.accessor;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.util.profiler.Profiler; // Importar Profiler
+import net.minecraft.util.profiler.Profiler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(MinecraftClient.class)
 public interface MinecraftClientAccessor {
-    @Accessor("profiler") // Assume que o campo se chama 'profiler' nos mapeamentos
-    Profiler getProfiler_(); // Nome do método com '_' para evitar conflitos, use este método no código
+    @Accessor("field_1729")
+    Profiler getProfiler_();
 }
