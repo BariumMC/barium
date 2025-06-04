75aaf917da990e2ca9e8151acb7d8eb35e825b72
PedrixzZDev <pedrixzz6@gmail.com>
2025-06-03 23:27:30 -0300

    Update ParticleManagerMixin.java

--- src/client/java/com/barium/client/mixin/ParticleManagerMixin.java
+++ src/client/java/com/barium/client/mixin/ParticleManagerMixin.java
@@ -4,36 +4,29 @@ import com.barium.client.optimization.ParticleOptimizer;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.LightmapTextureManager;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.world.ClientWorld;
+import net.minecraft.client.render.VertexConsumer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-/**
- * Mixin para o ParticleManager para otimizar a renderização de partículas.
- */
 @Mixin(ParticleManager.class)
 public class ParticleManagerMixin {
 
-    @Inject(
+    /**
+     * Redireciona a chamada para buildGeometry.
+     * Só renderiza partículas próximas.
+     */
+    @Redirect(
         method = "renderParticles(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;Lnet/minecraft/client/render/LightmapTextureManager;Lnet/minecraft/client/render/Camera;F)V",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/client/particle/Particle;buildGeometry(Lnet/minecraft/client/render/VertexConsumer;Lnet/minecraft/client/render/Camera;F)V"
-        ),
-        cancellable = true,
-        locals = LocalCapture.CAPTURE_FAILHARD
+        )
     )
-    private void barium$beforeBuildGeometry(MatrixStack matrices, VertexConsumerProvider.Immediate consumers, LightmapTextureManager lightmap, Camera camera, float tickDelta, CallbackInfo ci, Particle particle) {
-        ClientWorld world = (ClientWorld) particle.getWorld();
-
-        if (!ParticleOptimizer.shouldRenderParticle(particle, camera, world)) {
-            ci.cancel();
+    private void barium$shouldRenderParticle(Particle particle, VertexConsumer vertexConsumer, Camera camera, float tickDelta) {
+        if (ParticleOptimizer.shouldRenderParticle(particle, camera)) {
+            particle.buildGeometry(vertexConsumer, camera, tickDelta);
         }
+        // Se não deve renderizar: ignora silenciosamente.
     }
 }
