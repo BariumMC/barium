a878959aff031de58a921f8c32dfa410d2340615
PedrixzZDev <pedrixzz6@gmail.com>
2025-06-05 00:53:51 +0000

    test

--- src/client/java/com/barium/client/mixin/ParticleMixin.java
+++ src/client/java/com/barium/client/mixin/ParticleMixin.java
@@ -4,7 +4,8 @@ import com.barium.client.optimization.ParticleOptimizer;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.VertexConsumer;
+// import net.minecraft.client.render.VertexConsumer; // Não é mais necessário
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,18 +25,7 @@ public abstract class ParticleMixin {
         }
     }
 
-    // Otimização para pular a renderização (construção da geometria) de partículas distantes
-    // Especifica a assinatura completa do método para evitar o aviso "Unable to determine descriptor"
-    @Inject(
-        method = "buildGeometry(Lnet/minecraft/client/render/VertexConsumer;Lnet/minecraft/client/render/Camera;F)V",
-        at = @At("HEAD"),
-        cancellable = true
-    )
-    private void barium$skipDistantRender(VertexConsumer vertexConsumer, Camera camera, float tickDelta, CallbackInfo ci) {
-        Particle self = (Particle)(Object)this;
-        // Usa a mesma lógica de shouldRenderParticle do ParticleOptimizer
-        if (!ParticleOptimizer.shouldRenderParticle(self, camera)) {
-            ci.cancel(); // Cancela a execução do método buildGeometry
-        }
-    }
+    // A injeção para 'buildGeometry' foi removida temporariamente
+    // devido a problemas de compilação com a assinatura do método.
+    // A otimização de renderização de partículas estará desativada por enquanto.
 }
