1a7d8e5449aa3ad72b828b0032e89775afb59a69
PedrixzZDev <pedrixzz6@gmail.com>
2025-08-06 19:11:54 +0000

    teste

--- src/client/java/com/barium/client/config/BariumModMenu.java
+++ src/client/java/com/barium/client/config/BariumModMenu.java
@@ -1,36 +1,16 @@
-package com.barium.client.mixin.gui;
+package com.barium.client.config;
 
-import com.barium.client.config.BariumVideoSettingsScreen;
-import net.minecraft.client.MinecraftClient; // <-- IMPORT ADICIONADO
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.option.OptionsScreen;
-import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 
-@Mixin(OptionsScreen.class)
-public class OptionsScreenMixin {
+public class BariumModMenu implements ModMenuApi {
 
     /**
-     * Redireciona a criação da tela de Opções de Vídeo.
+     * Quando o botão de config do Barium for clicado no ModMenu,
+     * ele agora abrirá nossa nova tela de configurações de vídeo unificada.
      */
-    @Redirect(
-        method = "createVideoOptionsScreen",
-        at = @At(
-            value = "NEW",
-            target = "Lnet/minecraft/client/gui/screen/option/VideoOptionsScreen;"
-        )
-    )
-    private VideoOptionsScreen barium$redirectToCustomVideoSettings(Screen parent, net.minecraft.client.option.GameOptions options) {
-        // Quando o jogo tenta criar 'new VideoOptionsScreen(...)', nós interceptamos
-        // e, em vez disso, retornamos uma classe "falsa" que, ao ser aberta,
-        // imediatamente se substitui pela nossa tela personalizada do Barium.
-        return new VideoOptionsScreen(parent, options) {
-            @Override
-            public void init() {
-                this.client.setScreen(BariumVideoSettingsScreen.build(this.parent));
-            }
-        };
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> BariumVideoSettingsScreen.build(parent);
     }
 }
