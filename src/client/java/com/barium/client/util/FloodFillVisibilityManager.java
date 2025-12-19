526101a27296bc86d0ce04fd88b26aa42b6ebab3
PedrixzZDev <pedrixzz6@gmail.com>
2025-12-19 18:09:29 +0000

    teste

--- src/client/java/com/barium/client/util/FloodFillVisibilityManager.java
+++ src/client/java/com/barium/client/util/FloodFillVisibilityManager.java
@@ -16,7 +16,7 @@ public class FloodFillVisibilityManager {
     private static final FloodFillVisibilityManager INSTANCE = new FloodFillVisibilityManager();
     public static FloodFillVisibilityManager getInstance() { return INSTANCE; }
 
-    private final AtomicReference<LongSet> visibleSectionKeys = new AtomicReference<>(new LongOpenHashSet());
+    private final AtomicReference<LongSet> visibleChunkKeys = new AtomicReference<>(new LongOpenHashSet());
     private Future<?> visibilityTask = null;
     private long lastUpdateTime = 0;
     // Intervalo reduzido para resposta mais rápida ao mover a câmera
